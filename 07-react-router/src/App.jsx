ab594cb381a16525747ca74be057804d0fbaa42e
Parth Sharma <ps802sharma@gmail.com>
2025-08-17 20:44:49 +0530

    Ch-09: React Router

---
+++ 07-react-router/src/App.jsx
@@ -0,0 +1,7 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
---
+++ 07-react-router/src/components/User.jsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+const User = () => {
+  const { id } = useParams();
+
+  return (
+    <div className="bg-gray-600 text-white text-3xl text-center">
+      User: {id}
+    </div>
+  );
+};
+
+export default User;
